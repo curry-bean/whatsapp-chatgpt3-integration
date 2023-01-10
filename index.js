df2b1bfde5d4cafcc1389197e8db1c5499557dc4
AlvinwachiraKE <70557443+curry-bean@users.noreply.github.com>
2023-01-10 09:17:30 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+const { Configuration, OpenAIApi } = require("openai");
+require('dotenv').config()
+
+const client = new Client();
+
+client.on('qr', (qr) => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.initialize();
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+client.on('message', message => {
+    console.log(message.body);
+
+    if(message.body.startsWith("#")) {
+        runCompletion(message.body.substring(1)).then(result => message.reply(result));
+    }
+});
+
+async function runCompletion (message) {
+    const completion = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: message,
+        max_tokens: 200,
+    });
+    return completion.data.choices[0].text;
+}
